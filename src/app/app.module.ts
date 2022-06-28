b37d324ddefb8311f4ccb95bc15630c7b7402669
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-06-28 11:21:43 +0100

    added gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,23 @@ import { ServicesComponent } from './services/services.component';
 import { FaqComponent } from './faq/faq.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+const config = {
+  apiKey: "AIzaSyAqjiWXZ5aTyAbaKl40eJnSF0zoe2A97Eg",
+  authDomain: "elite-reserve-342019.firebaseapp.com",
+  databaseURL: "https://elite-reserve-342019-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "elite-reserve-342019",
+  storageBucket: "elite-reserve-342019.appspot.com",
+  messagingSenderId: "1040129815084",
+  appId: "1:1040129815084:web:61ad9fc50a605a3bc51789",
+  measurementId: "G-T7YZNN4JC4"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +43,11 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {collection, getDocs, getFirestore, query} from "firebase/firestore"
+
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  getAlbums = async () => {
+    // const q = query(collection(db, "cities"), where("capital", "==", true));
+
+    const q = query(collection(getFirestore(), "albums"))
+    const querySnapshot = await getDocs(q);
+    // querySnapshot.forEach((doc) => {
+    //     // doc.data() is never undefined for query doc snapshots
+    //     console.log(doc.id, " => ", doc.data());
+    // });
+    return querySnapshot
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.getAlbums().then(res=>{
+      console.log(res)
+    })
+
+    console.log("chujec kamulec")
   }
+
 
 }
