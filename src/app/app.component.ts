fe344869e4e57bb98210a6888630b71ea33ae773
kbskdev <milosz.olsztyn@gmail.com>
2022-06-30 15:47:29 +0100

    mobilka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import {AppRoutingModule} from "./app-routing.module";
 
 @Component({
@@ -6,8 +6,10 @@ import {AppRoutingModule} from "./app-routing.module";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photo:string = "../../assets/sligo1.jpg"
+export class AppComponent implements OnInit{
+  photo:string = "../../assets/polonia2.jpg"
+
+  photos:string[] = ["../../assets/polonia2.jpg","../../assets/chujec_kamulec.jpg","../../assets/chujec_kamulec2.jpg","../../assets/chujec_kamulec3.jpg"]
 
 
   title = 'sligowebsite';
@@ -15,6 +17,18 @@ export class AppComponent {
   homeboll:boolean = true;
 
   list:boolean[] = [false,false,false,false,false,false]
+
+  counter:number = 1
+
+  changer(){
+    setInterval(()=>{
+      this.counter++
+      if(this.counter>=this.photos.length){
+        this.counter=0
+      }
+      this.photo = this.photos[this.counter]
+    },7000)
+  }
 
   shower(id:number){
     for(let i=0;i<=6;i++){
@@ -24,5 +38,8 @@ export class AppComponent {
     }
     this.list[id] = !this.list[id];
     this.homeboll = !this.list[id];
+  }
+  ngOnInit() {
+    this.changer()
   }
 }
